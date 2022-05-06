5b710618a9697e7fbc5f46050274b984cefb0328
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-05-06 17:19:26 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit5-06-HTML
+// May 6 2022
 
 'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-06-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-06-HTML/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Calculates the product of two numbers the user provides.
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  var numberOne = parseInt(document.getElementById("numberOne").value)
+  var numberTwo = parseInt(document.getElementById("numberTwo").value)
+  var result = 0
+
+  if (numberOne != 0 && numberTwo >= 0) {
+    while (numberTwo > 0) {
+      result = result + numberOne
+      numberTwo = numberTwo - 1
+    }
+    console.log("Number 1 is negative or postive, and Number 2 is positive.")
+  } else if (numberTwo < 0) {
+    while (numberTwo != 0) {
+      result = result - numberOne
+      numberTwo = numberTwo + 1
+    }
+    console.log("Number 1 is negative or postive, and Number 2 is negative.")
+  }
+  document.getElementById("result").innerHTML = "<h5>The product of these numbers is " + result + "</h5>"
 }
